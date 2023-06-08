9564c8ca9724634b1698228f5c4408b72d1d006d
gabriela712 <57546730+gabriela712@users.noreply.github.com>
2023-06-09 01:27:25 +0200

    Calculadora descuento

---
+++ Clase5/CalculadoraDescuento.java
@@ -0,0 +1,25 @@
+package Clase5;
+
+import java.util.Scanner;
+public class CalculadoraDescuento {
+    public static void main(String[] args) {
+        Scanner cargaDeDatos= new Scanner(System.in);
+
+        System.out.print("Ingrese el precio: ");
+        double precio = cargaDeDatos.nextDouble();
+
+        System.out.print("Ingrese el porcentaje de descuento: ");
+        double descuento = cargaDeDatos.nextDouble();
+
+        double precioFinal = calcularPrecioFinal(precio, descuento);
+
+        System.out.println("Precio final despues del descuento: " + precioFinal);
+    }
+    public static double calcularPrecioFinal(double precio, double descuento) {
+        double porcentajeDescuento = descuento / 100;
+        double montoDescuento = precio * porcentajeDescuento;
+        double precioFinal = precio - montoDescuento;
+
+        return precioFinal;
+    }
+}
