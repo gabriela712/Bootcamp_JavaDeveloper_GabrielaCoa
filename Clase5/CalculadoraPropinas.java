07384255de4089897b1069b1a7ab01a51d9e0d02
gabriela712 <57546730+gabriela712@users.noreply.github.com>
2023-06-09 02:37:56 +0200

    Calculadora propinas

---
+++ Clase5/CalculadoraPropinas.java
@@ -0,0 +1,8 @@
+package Clase5;
+
+import java.util.Scanner;
+public class CalculadoraPropinas {
+    public static void main(String[] args) {
+        Scanner cargaDeDatos = new Scanner(System.in);
+
+        System.out.print("Ingrese el total de la cuenta");
