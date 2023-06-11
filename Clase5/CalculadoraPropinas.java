b236b088437cad5862a1bd3d463df226ba81b0d0
gabriela712 <57546730+gabriela712@users.noreply.github.com>
2023-06-11 18:56:02 +0200

    Juego Adivinar El Numero

--- Clase5/CalculadoraPropinas.java
+++ Clase5/CalculadoraPropinas.java
@@ -5,4 +5,18 @@ public class CalculadoraPropinas {
     public static void main(String[] args) {
         Scanner cargaDeDatos = new Scanner(System.in);
 
-        System.out.print("Ingrese el total de la cuenta");
+        System.out.println("Ingrese el total de la cuenta: ");
+        double montoCuenta = cargaDeDatos.nextDouble();
+
+        System.out.println("Ingrese el porcentaje de propina: ");
+        double porcentajePropina = cargaDeDatos.nextDouble();
+
+        double propina = montoCuenta * (porcentajePropina / 100);
+        double totalPago = montoCuenta + propina;
+
+        System.out.println("Monto de la cuenta: $" + montoCuenta);
+        System.out.println("Propina: $" + propina);
+        System.out.println("Total a pagar: $" + totalPago);
+    }
+
+}
---
+++ Clase5/JuegoAdiivinarNumero.java
@@ -0,0 +1,34 @@
+package Clase5;
+
+import java.util.Random;
+import java.util.Scanner;
+public class JuegoAdiivinarNumero {
+    public static void main(String[] args){
+        Random random = new Random();
+        int numeroAleatorio = random.nextInt(100) + 1;
+
+        Scanner cargaDeDatos = new Scanner(System.in);
+        int intentos = 0;
+        int numeroAdivinado;
+
+
+        System.out.println("Bienvenido al juego de adivinar el numero!");
+        System.out.println("Adivina el numero del 1 al 100: ");
+
+        do {
+            intentos++;
+            System.out.print("Intento #" + intentos + ": ");
+            numeroAdivinado = cargaDeDatos.nextInt();
+
+            if (numeroAdivinado < numeroAleatorio) {
+                System.out.println("El número es mayor.");
+            } else if (numeroAdivinado > numeroAleatorio){
+                System.out.println("El numero es menor.");
+            } else {
+                System.out.println("¡Felicidades! ¡Has adivinado el numero en " + intentos + " intentos" );
+            }
+        } while (numeroAdivinado != numeroAleatorio);
+
+        System.out.println("Fin del juego. Gracias por jugar.");
+    }
+}
